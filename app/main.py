71a4b0e943d07b8609913f8775bf3ddc2e839007
otoshisan <greenmonstar2018@gmail.com>
2026-02-09 14:40:07 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-import os
-from openai import OpenAI
 import requests
 
-GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxNzqeepKifrMa79MXHwMmvFows9alWKBDP1aY56ioY4v2Q3TJifcps6WfR_1nAW67ieA/exec"
+GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxyZkOkQUYE0M7aIyRz0SrXTYM9XS11gT7NrEkcijaD12yE-E_tYyKrLQbERrk6a3PY3A/exec"
 
 def create_reservation(data: dict):
-    response = requests.post(GAS_ENDPOINT, json=data)
-    return response.json()
+    response = requests.post(
+        GAS_ENDPOINT,
+        json=data,
+        headers={"Content-Type": "application/json"}
+    )
+    return response.text
+
 
 if __name__ == "__main__":
     reservation = {
@@ -19,18 +22,3 @@ if __name__ == "__main__":
 
     result = create_reservation(reservation)
     print(result)
-
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-def ask_ai(question: str) -> str:
-    response = client.responses.create(
-        model="gpt-4.1-mini",
-        input=question
-    )
-    return response.output_text
-
-if __name__ == "__main__":
-    user_input = input("質問を入力してください: ")
-    answer = ask_ai(user_input)
-    print("AIの回答:", answer)
